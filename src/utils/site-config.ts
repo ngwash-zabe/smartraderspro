d8631d216836d926a57ae9f7b5e9941daba6919c
ngwash-zabe <itsshon57@gmail.com>
2026-07-02 07:50:45 -0400

    Rebrand: update site-config.ts

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -1,4 +1,4 @@
-export const website_domain = 'app.deriv.com';
-export const website_name = 'Deriv';
+export const website_domain = 'shonfx.site';
+export const website_name = 'SHONFX';
 export const default_title = website_name;
 export const TRACKING_STATUS_KEY = 'tracking_status';
