13169a3bfeab4cbbd6c8f031be4ac419f527e6e1
ngwash-zabe <itsshon57@gmail.com>
2026-07-02 07:50:54 -0400

    Rebrand: update Deriv APP ID & OAUTH config

--- src/components/shared/utils/config/config.ts
+++ src/components/shared/utils/config/config.ts
@@ -1,29 +1,17 @@
-import { LocalStorageConstants, LocalStorageUtils, URLUtils } from '@deriv-com/utils';
-import { isStaging } from '../url/helpers';
-
 export const APP_IDS = {
     LOCALHOST: 36300,
-    TMP_STAGING: 64584,
     STAGING: 29934,
-    STAGING_BE: 29934,
-    STAGING_ME: 29934,
-    PRODUCTION: 65555,
-    PRODUCTION_BE: 65556,
-    PRODUCTION_ME: 65557,
+    PRODUCTION: '33IaWAuFIPhXbuKxWWjmm',
+};
+
+export const domain_app_ids = {
+    'shonfx.site': APP_IDS.PRODUCTION,
+    'www.shonfx.site': APP_IDS.PRODUCTION,
+    'staging-dbot.deriv.com': APP_IDS.STAGING,
 };
 
 export const livechat_license_id = 12049137;
 export const livechat_client_id = '66aa088aad5a414484c1fd1fa8a5ace7';
-
-export const domain_app_ids = {
-    'master.bot-standalone.pages.dev': APP_IDS.TMP_STAGING,
-    'staging-dbot.deriv.com': APP_IDS.STAGING,
-    'staging-dbot.deriv.be': APP_IDS.STAGING_BE,
-    'staging-dbot.deriv.me': APP_IDS.STAGING_ME,
-    'dbot.deriv.com': APP_IDS.PRODUCTION,
-    'dbot.deriv.be': APP_IDS.PRODUCTION_BE,
-    'dbot.deriv.me': APP_IDS.PRODUCTION_ME,
-};
 
 export const getCurrentProductionDomain = () =>
     !/^staging\./.test(window.location.hostname) &&
